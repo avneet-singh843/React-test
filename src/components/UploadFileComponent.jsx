e746ccd2d78d38f96f3198efe74eb70782310d66
Avneet Singh <avneetsingh843@gmail.com>
2023-05-10 01:19:46 +0530

    added upload file component

--- src/components/UploadFileComponent.jsx
+++ src/components/UploadFileComponent.jsx
@@ -1,7 +1,16 @@
+import "./UploadFileComponent.css"
+import UploadFileIcon from '@mui/icons-material/UploadFile';
 const UploadFileComponent = () => {
     return (
         <>
-            <h1>upload file</h1>
+            <div className="dropBox">
+                <input type="file" className="uploadHide" id="uploadCustom" />
+                <label for="uploadCustom" className="uploadLabel">
+                    <UploadFileIcon style={{ fontSize: "10rem", color: "#2F7EC7" }} />
+                    <button className="chooseFiles">Browse Files</button>
+                    <p className="dragText">or drop files here</p>
+                </label>
+            </div>
         </>
     );
 }
