6c10690c1939d38d826ee1b22bfbcb40b10b751b
Avneet Singh <avneetsingh843@gmail.com>
2023-05-10 23:35:13 +0530

    designed save&preview

--- src/components/SavePreviewComponent.jsx
+++ src/components/SavePreviewComponent.jsx
@@ -1,8 +1,33 @@
+import React from 'react';
+import schema from '../assets/schema.json'
+import './MapChannelsComponent.css'
+import './SavePreviewComponent.css'
+import PrimaryChannelButton from './PrimaryChannelButton';
 const SavePreviewComponent = () => {
     return (
-        <>
-            <h1>save & preview</h1>
-        </>
+        <div className='table'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Channels</th>
+                        <th>Primary Channel</th>
+                        <th>Reference Channel</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {schema.channels.map((channel, index) => (
+                        <tr key={index}>
+                            <td>{channel}</td>
+                            <td><PrimaryChannelButton /></td>
+                            <td><PrimaryChannelButton /></td>
+                            <td><button className='edit'>Edit Channel</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
+
 export default SavePreviewComponent;
