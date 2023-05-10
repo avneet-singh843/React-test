bc4ebe4aaa47bffd1d39f46754980194d5191e0d
Avneet Singh <avneetsingh843@gmail.com>
2023-05-10 20:42:50 +0530

    added functionality to next, back, cancel buttons

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,15 +1,32 @@
+import React, { Component } from 'react';
+import Component1 from "../components/UploadFileComponent";
+import Component2 from "../components/MapChannelsComponent";
 import "./FooterComponent.css"
-const FooterComponent = () => {
-    return (
-        <div className="footer">
-            <div className="left">
-                <button className="back">Back</button>
-                <button className="cancel">Cancel Montage</button>
+window.index = 1;
+
+class FooterComponent extends Component {
+    handleNextClick = () => {
+        window.index++;
+        console.log(window.index);
+    }
+
+    handleBackClick = () => {
+        window.index--;
+        console.log(window.index);
+    }
+    render() {
+        return (
+            <div className="footer">
+                <div className="left">
+                    <button className="back" onClick={this.handleBackClick}>Back</button>
+                    <button className="cancel" onClick={() => { console.log("cancel") }}>Cancel Montage</button>
+                </div>
+                <div className="right">
+                    <button className="nextComponent" onClick={this.handleNextClick}>Next</button>
+                </div>
             </div>
-            <div className="right">
-                <button className="next">Next</button>
-            </div>
-        </div>
-    );
+        );
+    }
 }
+
 export default FooterComponent;
--- src/pages/MontagesPage.jsx
+++ src/pages/MontagesPage.jsx
@@ -1,30 +1,62 @@
-import FooterComponent from "../components/FooterComponent";
+import React, { useState } from 'react';
+import "./pages.css";
+import "../components/FooterComponent.css"
 import HeaderComponent from "../components/HeaderComponent";
-import SavePreviewComponent from "../components/SavePreviewComponent";
+import Component3 from "../components/SavePreviewComponent";
 import SideBarComponent from "../components/SideBarComponent";
 import StepperComponent from "../components/StepperComponent";
-import UploadFileComponent from "../components/UploadFileComponent";
-import MapChannelsComponent from "../components/MapChannelsComponent";
+import Component1 from "../components/UploadFileComponent";
+import Component2 from "../components/MapChannelsComponent";
 
-
-import "./pages.css"
 const MontagesPage = () => {
+    const [index, setIndex] = useState(1);
+
+    const handleNextClick = () => {
+        setIndex(prevIndex => prevIndex + 1);
+    }
+
+    const handleBackClick = () => {
+        setIndex(prevIndex => prevIndex - 1);
+    }
+
+    const handleCancelClick = () => {
+        setIndex(prevIndex => 1);
+    }
+
+    const displayComponent = () => {
+        if (index === 1) {
+            return <Component1 />;
+        }
+        if (index === 2) {
+            return <Component2 />;
+        }
+        if (index === 3) {
+            return <Component3 />;
+        }
+        return null;
+    }
+
     return (
-        <>
-            <div className="content">
-                <div className="sidebarContent">
-                    <SideBarComponent />
-                </div>
-                <div className="mainContent">
-                    <HeaderComponent />
-                    <StepperComponent />
-                    <UploadFileComponent />
-                    <FooterComponent />
-                    <MapChannelsComponent />
-                    <SavePreviewComponent />
+        <div className="content">
+            <div className="sidebarContent">
+                <SideBarComponent />
+            </div>
+            <div className="mainContent">
+                <HeaderComponent />
+                <StepperComponent />
+                {displayComponent()}
+                <div className="footer">
+                    <div className="left">
+                        <button disabled={index <= 1} className="back" onClick={handleBackClick}>Back</button>
+                        <button className="cancel" onClick={handleCancelClick}>Cancel Montage</button>
+                    </div>
+                    <div className="right">
+                        <button disabled={index >= 3} className="nextComponent" onClick={handleNextClick}>Next</button>
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
+
 export default MontagesPage;
